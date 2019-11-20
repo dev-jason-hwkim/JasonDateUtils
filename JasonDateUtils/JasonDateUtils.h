f4f7f26c53a778da642228d5eae3f1a26fcfbc0a
jason <jason@kidsnote.com>
2019-11-20 10:35:44 +0900

    Initial Commit

---
+++ JasonDateUtils/JasonDateUtils.h
@@ -0,0 +1,19 @@
+//
+//  JasonDateUtils.h
+//  JasonDateUtils
+//
+//  Created by Jason on 20/11/2019.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JasonDateUtils.
+FOUNDATION_EXPORT double JasonDateUtilsVersionNumber;
+
+//! Project version string for JasonDateUtils.
+FOUNDATION_EXPORT const unsigned char JasonDateUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JasonDateUtils/PublicHeader.h>
+
+
